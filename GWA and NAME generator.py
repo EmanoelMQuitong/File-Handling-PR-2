082fcdaee0921fd527439ba8b623822554a0c001
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-04-27 22:30:24 +0800

    Random GWA with names generators established

--- GWA and NAME generator.py
+++ GWA and NAME generator.py
@@ -1,17 +1,10 @@
-#TASK: P-2 (25 points). Write a Python program that read a file containing the name of 20 students together with their GWA. The program will outputs the name of the student who got the highest GWA (including the GWA).
-
-#Write a Python program that read a file containing the name of 20 students together with their GWA.
-
-#The program will outputs the name of the student who got the highest GWA (including the GWA).
-
-#Generate numbers for GWA
 import random as rd
 
-NAMES = open('names.txt', 'r')
+NAMES = open('names.txt', 'r')  #Open contents that consists of Names from names.txt
 for line in NAMES:
-    num = rd.uniform(1,5) 
-    num = "%.2f"%num
+    num = rd.uniform(1,5)       #Generate numbers for GWA
+    num = "%.2f"%num            #Generate numbers for GWA
     GWA_NAMES = open('GWA_and_NAME.txt', 'a')
-    GWA_NAMES.write(num), GWA_NAMES.write(" "), GWA_NAMES.write(line)
+    GWA_NAMES.write(num), GWA_NAMES.write(" "), GWA_NAMES.write(line) #Append contents from names.txt and random GWA.
     
 
