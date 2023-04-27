d70c1639c45fb90929275fb6447bbf54a1e548ac
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-04-27 20:18:23 +0800

    Names has been added to the files as names.txt. Random GWA generator is created for the students GWA.

--- FILE-HANDLING-PR-2.py
+++ FILE-HANDLING-PR-2.py
@@ -3,3 +3,11 @@
 #Write a Python program that read a file containing the name of 20 students together with their GWA.
 
 #The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+#Generate numbers for GWA
+import random as rd
+def gwa(num):
+    for i in range(20):
+        num = rd.uniform(1.00,5.00)
+        print("%.2f" % num)
+    return num
